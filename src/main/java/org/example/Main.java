c67d2c4cda0d4db16863cf089f4b3b0b3772e81a
jasonko <f2675805@gmail.com>
2025-10-28 17:23:37 +0800

    資料流

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,10 @@ package org.example;
 import com.hr.Employee;
 import com.hr.Manager;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -19,6 +22,7 @@ public class Main {
         Manager manager = new Manager("八嘎" , 70000 , 1000);
         manager.showInfo();
 
+        /*
         Employee[] employees = { employee, manager };
 
         //  一般寫法
@@ -29,5 +33,25 @@ public class Main {
         Arrays.stream(employees).forEach(e ->
                 System.out.println(e.getName() + " 年薪: " + e.calculateAnnualSalary())
         );
+        */
+
+        //  使用 ArrayList 取代陣列
+        List<Employee> employees = new ArrayList<>();
+        employees.add(employee);
+        employees.add(manager);
+        //  Lambda + Stream 印出薪資
+        employees.stream().forEach(e -> System.out.println(e.getName() + " " + e.getPosition() + " " + e.calculateAnnualSalary()));
+
+        //  Method Reference 簡化寫法
+        employees.stream().map(Employee::toString).forEach(System.out::println);
+
+        List<String> names = employees.stream()
+                .filter(e -> e.calculateAnnualSalary() > 600000)
+                .map(Employee::getName)
+                .sorted()
+                .distinct()
+                .collect(Collectors.toList());
+
+        names.forEach(System.out::println);
     }
 }
